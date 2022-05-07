7667080ad687170a32f8ca3b1071e72639944ecc
Matias Pujado <matiaspujado@gmail.com>
2022-05-07 13:16:47 -0300

    Ex8 incomplete

--- Class-3/src/sumofconsecnumb/Main.java
+++ Class-3/src/sumofconsecnumb/Main.java
@@ -2,31 +2,18 @@ package sumofconsecnumb;
 
 /**
  * 8 - NO OBLIGATORIO (Es para pensar): Metodo que devuelva un boolean indicando si esta formado por la suma de numeros consecutivos menores.
- * Ej:
- * firma --> boolean esSumatoriaDeConsecutivos(int value);
+ *     Ej:
+ *        firma --> boolean esSumatoriaDeConsecutivos(int value);
  *
- * esSumatoriaDeConsecutivos(3) = TRUE pq 3 = 2 + 1
- * esSumatoriaDeConsecutivos(9) = TRUE pq 9 = 4 + 3 + 2
+ *        esSumatoriaDeConsecutivos(3) = TRUE pq 3 = 2 + 1
+ *        esSumatoriaDeConsecutivos(9) = TRUE pq 9 = 4 + 3 + 2
  *
- * Se puede implementar recursivo y iterativo.  Deseable contar con metodo que devuelva la lista de numeros que representan la suma.
- * Comparar performance de ambas implementaciones y sacar conclusiones.
+ *     Se puede implementar recursivo y iterativo.  Deseable contar con metodo que devuelva la lista de numeros que representan la suma.
+ *     Comparar performance de ambas implementaciones y sacar conclusiones.
  */
-
 
 public class Main {
     public static void main(String[] args) {
 
-    }
-
-    public static boolean recursiveConsNumbers(int num) {
-        // Base case
-
-        
-        return false;
-    }
-
-    public static boolean iterativeConsNumbers() {
-
-        return false;
     }
 }
